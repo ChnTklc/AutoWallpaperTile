706a59729699998461bb24480ce7398943de5d7f
Cihan Toklucu <cihantoklucu2@outlook.com>
2017-11-23 21:48:36 +0300

    wallpapaer tile action done.

--- app/src/main/java/usage/personal/wallpapertile/WallpaperTile.kt
+++ app/src/main/java/usage/personal/wallpapertile/WallpaperTile.kt
@@ -5,6 +5,7 @@ import android.graphics.BitmapFactory
 import android.service.quicksettings.TileService
 import android.util.Log
 import java.io.File
+import java.util.Random
 
 /**
  * Edited by Comteng on 22/11/2017.
@@ -19,12 +20,14 @@ class WallpaperTile: TileService() {
             val wpManager = WallpaperManager.getInstance(applicationContext)
             val choosenFolder =  File(MainActivity.getKEY(applicationContext, MainActivity.pathKey).toString())
             Log.i(tag, "Choosen Folder Path: "+ choosenFolder.path)
-
-            // TODO: from choosenFolder take random image and put into newWP
-
-            val newWP = BitmapFactory.decodeFile("")
+            val imagesList = choosenFolder.listFiles()
+            val randomNumber = (0..imagesList.size).random()
+            Log.i(tag, "Choosen Image Path: "+ imagesList[randomNumber].absolutePath)
+            val newWP = BitmapFactory.decodeFile(imagesList[randomNumber].absolutePath)
             wpManager.setBitmap(newWP)
             qsTile.updateTile()
         }.run()
     }
+
+    private fun ClosedRange<Int>.random() = Random().nextInt(endInclusive - start) +  start
 }
